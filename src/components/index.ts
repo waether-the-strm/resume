8f2afbc3e6971494301fa8739a1d775fe497ccb9
Jan Mirecki <mirecki.janek@gmail.com>
2024-08-05 14:26:56 +0200

    Init astro cv

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+export { default as Header } from "./Header.astro";
+export { default as Education } from "./Education.astro";
+export { default as Experience } from "./Experience.astro";
+export { default as TechStack } from "./TechStack.astro";
+export { default as Contact } from "./Contact.astro";
