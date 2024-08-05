0b1e50c3ec7ffcc78a25a8ff2bf1a407b93b785c
Jan Mirecki <mirecki.janek@gmail.com>
2024-08-05 19:57:49 +0200

    Implement layout

---
+++ src/shared/classes.ts
@@ -0,0 +1,5 @@
+export const classes = {
+  h1: "text-3xl text-white/90 font-bold leading-8 mb-5 print:text-slate-900",
+  h2: "text-lg lowercase text-gray-300 border-b border-slate-800 border-dotted pb-2 mb-6 print:mb-0 print:text-slate-900",
+  h3: "text-sm text-slate-300 mb-5 relative transition-opacity print:text-slate-900",
+};
