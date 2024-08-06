da74829fe70c3860202df300d1ac9b71f09bc9d5
Jan Mirecki <mirecki.janek@gmail.com>
2024-08-06 21:03:02 +0200

    Configure github pages for astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), tailwind(), react()]
+  integrations: [mdx(), tailwind(), react()],
+  site: "https://waether-the-strm.github.io",
+  base: "resume",
 });
