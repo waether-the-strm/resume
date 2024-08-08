3c84ed3e8460ef66eb99aa07a8f9b9914516b031
Jan Mirecki <mirecki.janek@gmail.com>
2024-08-08 21:49:21 +0200

    Update for gtag and scripts

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,4 +4,15 @@ export { default as Experience } from "./Experience.astro";
 export { default as TechStack } from "./TechStack.astro";
 export { default as Contact } from "./Contact.astro";
 export { default as Languages } from "./Languages.astro";
-export { default as Scripts } from "./tags/Scripts.astro";
+
+import { default as GTM } from "./scripts/GTM.astro";
+import { default as GTMns } from "./scripts/GTMns.astro";
+import { default as GA } from "./scripts/GA.astro";
+import { default as Lenis } from "./scripts/Lenis.astro";
+
+export const Scripts = {
+  GTM,
+  GTMns,
+  GA,
+  Lenis,
+};
